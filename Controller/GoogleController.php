d49d19a4ef859fc7c34a34cd2302039c836dd07d
Nikita Makarov <nikita.makarov@effective-soft.com>
2015-02-12 16:31:36 +0300

    Controllers now are ABSTRACTED

--- Controller/GoogleController.php
+++ Controller/GoogleController.php
@@ -2,30 +2,12 @@
 
 namespace Akuma\Bundle\SocialBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Intl\Exception\MethodNotImplementedException;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-
 /**
- * Class FacebookController
+ * Class GoogleController
  *
  * @package Akuma\Bundle\SocialBundle\Controller
- * @Route("")
  */
-class GoogleController extends Controller
+class GoogleController extends AbstractController
 {
-    /**
-     * @Route("/connect")
-     *
-     * @Method({"GET", "POST"})
-     *
-     * @throws \Symfony\Component\Intl\Exception\MethodNotImplementedException
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function connectAction()
-    {
-        throw new MethodNotImplementedException(__METHOD__);
-    }
+
 }
--- Controller/MicrosoftController.php
+++ Controller/MicrosoftController.php
@@ -2,30 +2,11 @@
 
 namespace Akuma\Bundle\SocialBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Intl\Exception\MethodNotImplementedException;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-
 /**
  * Class FacebookController
  *
  * @package Akuma\Bundle\SocialBundle\Controller
- * @Route("")
  */
-class MicrosoftController extends Controller
+class MicrosoftController extends AbstractController
 {
-    /**
-     * @Route("/connect")
-     *
-     * @Method({"GET", "POST"})
-     *
-     * @throws \Symfony\Component\Intl\Exception\MethodNotImplementedException
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function connectAction()
-    {
-        throw new MethodNotImplementedException(__METHOD__);
-    }
 }
